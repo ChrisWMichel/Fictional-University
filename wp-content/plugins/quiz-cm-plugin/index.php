46ae951730eebc41aa2625e3df749792aadfa4f4
ChrisWMichel <christopherw.michel@gmail.com>
2026-03-25 16:49:49 -0500

    Update quiz plugin

--- wp-content/plugins/quiz-cm-plugin/index.php
+++ wp-content/plugins/quiz-cm-plugin/index.php
@@ -16,19 +16,24 @@ class QuizCMPlugin {
     }
 
     public function adminAssets() {
-        //$frontend_asset = require plugin_dir_path(__FILE__) . 'build/frontend.asset.php';
-        // wp_register_style('quiz-cm-css', plugin_dir_url(__FILE__) . 'build/index.css', array(), '1.0');
-         wp_register_style('quiz-cm-frontend-css', plugin_dir_url(__FILE__) . 'build/frontend.css', array(), '1.0');
-        // wp_register_script('quiz-cm-admin', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'), '1.0', true);
-        //wp_register_script('quiz-cm-frontend', plugin_dir_url(__FILE__) . 'build/frontend.js', $frontend_asset['dependencies'], $frontend_asset['version'], true);
-        register_block_type(__DIR__, array(
+        $frontend_asset = require plugin_dir_path(__FILE__) . 'build/frontend.asset.php';
+        $admin_asset = require plugin_dir_path(__FILE__) . 'build/index.asset.php';
+
+        wp_register_style('quiz-cm-css', plugin_dir_url(__FILE__) . 'build/index.css', array(), $admin_asset['version']);
+        wp_register_style('quiz-cm-frontend-css', plugin_dir_url(__FILE__) . 'build/frontend.css', array(), $frontend_asset['version']);
+        wp_register_script('quiz-cm-admin', plugin_dir_url(__FILE__) . 'build/index.js', $admin_asset['dependencies'], $admin_asset['version'], true);
+        wp_register_script('quiz-cm-frontend', plugin_dir_url(__FILE__) . 'build/frontend.js', $frontend_asset['dependencies'], $frontend_asset['version'], true);
+
+        register_block_type('quiz-cm-plugin/quiz-block', array(
+            'editor_script' => 'quiz-cm-admin',
+            'editor_style'  => 'quiz-cm-css',
             'render_callback' => array($this, 'renderQuizBlock'),
         ));
     }
 
     public function renderQuizBlock($attributes) {
-        // wp_enqueue_script('quiz-cm-frontend');
-       // wp_enqueue_style('quiz-cm-frontend-css',  plugin_dir_url(__FILE__) . 'build/frontend.css', array(), '1.0');
+        wp_enqueue_script('quiz-cm-frontend');
+        wp_enqueue_style('quiz-cm-frontend-css');
         ob_start();
         ?>
         <div class="paying-attention-quiz-update">
